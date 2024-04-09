2f7489b2e0c212468615c3e03bd27bbd799c7fbd
XBXyftx <145579105+XBXyftx@users.noreply.github.com>
2024-04-09 17:13:52 +0800

    Add files via upload

---
+++ entry/build/default/cache/default/default@CompileArkTS/esmodule/debug/entry/src/main/ets/pages/page2.js
@@ -0,0 +1,111 @@
+"use strict";
+class Second extends ViewPU {
+    constructor(parent, params, __localStorage, elmtId = -1) {
+        super(parent, __localStorage, elmtId);
+        this.__message = new ObservedPropertySimplePU('波奇酱', this, "message");
+        this.setInitiallyProvidedValue(params);
+    }
+    setInitiallyProvidedValue(params) {
+        if (params.message !== undefined) {
+            this.message = params.message;
+        }
+    }
+    updateStateVars(params) {
+    }
+    purgeVariableDependenciesOnElmtId(rmElmtId) {
+        this.__message.purgeDependencyOnElmtId(rmElmtId);
+    }
+    aboutToBeDeleted() {
+        this.__message.aboutToBeDeleted();
+        SubscriberManager.Get().delete(this.id__());
+        this.aboutToBeDeletedInternal();
+    }
+    get message() {
+        return this.__message.get();
+    }
+    set message(newValue) {
+        this.__message.set(newValue);
+    }
+    initialRender() {
+        this.observeComponentCreation((elmtId, isInitialRender) => {
+            ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+            Row.create();
+            Row.height('100%');
+            Row.backgroundImage({ "id": 0, "type": 30000, params: ['pochi.jpg'], "bundleName": "com.example.mya", "moduleName": "entry" });
+            Row.backgroundImageSize(ImageSize.Cover);
+            Row.backgroundImagePosition(Alignment.Center);
+            if (!isInitialRender) {
+                Row.pop();
+            }
+            ViewStackProcessor.StopGetAccessRecording();
+        });
+        this.observeComponentCreation((elmtId, isInitialRender) => {
+            ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+            Column.create();
+            Column.width('100%');
+            if (!isInitialRender) {
+                Column.pop();
+            }
+            ViewStackProcessor.StopGetAccessRecording();
+        });
+        this.observeComponentCreation((elmtId, isInitialRender) => {
+            ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+            Text.create(this.message);
+            Text.fontSize(50);
+            Text.fontWeight(FontWeight.Regular);
+            Text.align(Alignment.Center);
+            Text.fontColor('#ffec00ff');
+            if (!isInitialRender) {
+                Text.pop();
+            }
+            ViewStackProcessor.StopGetAccessRecording();
+        });
+        Text.pop();
+        this.observeComponentCreation((elmtId, isInitialRender) => {
+            ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+            Row.create();
+            Row.width('100.00%');
+            Row.height('20.00%');
+            Row.align(Alignment.Center);
+            if (!isInitialRender) {
+                Row.pop();
+            }
+            ViewStackProcessor.StopGetAccessRecording();
+        });
+        this.observeComponentCreation((elmtId, isInitialRender) => {
+            ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+            Navigator.create({ target: "pages/Index", type: NavigationType.Push });
+            Navigator.width('40.00%');
+            Navigator.height('40.00%');
+            Navigator.backgroundColor('#d4e01212');
+            Navigator.align(Alignment.Center);
+            Navigator.position({ "x": "30.00%", "y": "0.00px" });
+            if (!isInitialRender) {
+                Navigator.pop();
+            }
+            ViewStackProcessor.StopGetAccessRecording();
+        });
+        this.observeComponentCreation((elmtId, isInitialRender) => {
+            ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+            Text.create('回到第一页');
+            Text.fontSize(25);
+            Text.align(Alignment.Center);
+            if (!isInitialRender) {
+                Text.pop();
+            }
+            ViewStackProcessor.StopGetAccessRecording();
+        });
+        Text.pop();
+        Navigator.pop();
+        Row.pop();
+        Column.pop();
+        Row.pop();
+    }
+    rerender() {
+        this.updateDirtyElements();
+    }
+}
+ViewStackProcessor.StartGetAccessRecordingFor(ViewStackProcessor.AllocateNewElmetIdForNextComponent());
+loadDocument(new Second(undefined, {}));
+ViewStackProcessor.StopGetAccessRecording();
+//# sourceMappingURL=page2.js.map
